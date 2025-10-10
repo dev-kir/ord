2e53df7fd803a57da57fc16281cd56194bee2a7a
amirmuz <secdev.kir@gmail.com>
2025-10-10 15:28:58 +0800

    refix server.js file to seamlessly connect with the frontend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 2306;
 
-app.use(cors({ origin: "http://localhost:5173" })); // or "*" for testing
+// app.use(cors({ origin: "http://localhost:5173" })); // or "*" for testing
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.set("trust proxy", true);
 
@@ -23,7 +24,7 @@ app.use("/api/count", countRoutes);
 app.use("/api/sentiment", sentimentRoutes);
 app.use("/api/stats", statsRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0,0,0,0", () => {
   connectDB();
   console.log("Server started at http://localhost:" + PORT);
 });
